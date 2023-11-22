31a3274d8f3a769835687c44ed50bad52cce0416
Neymarsjs <120604585+Neymarsjs@users.noreply.github.com>
2023-11-22 14:05:04 -0300

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ import moment from 'moment-timezone'
 
 //============= OWMER ============= 
 global.owner = [
-  ['5218442286089', '👑 DIEGO-OFC 👑', true],
+  ['59897355923', '👑 DIEGO-OFC 👑', true],
   ['5219992095479', '👑 BRUNO-SOBRINO', true]
 ] 
 //Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
